9d6b7fe0edd1b8927dc5aa5a380e5054ce8fec89
Tlotliso Mpeoa <Tlotliso2062@gmail.com>
2025-04-07 17:40:19 +0200

    Updated vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/Kompyler/',  // Set the base path for your project
   plugins: [vue()],
   define: {
-    '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false
+    '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false  // Prevents hydration mismatch warnings
   }
 });
